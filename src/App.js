cae768bda130c6b1afb2df5b8f635ce94437e8e3
Fabou78 <fpaz@toto.com>
2019-01-28 02:45:32 +0000

    Completed form and behaviour without result calculation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-import { Button } from 'react-bulma-components';
+import Profit from './components/Profit';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Button color="primary">My Bulma button</Button>
+        <Profit />
       </div>
     );
   }
