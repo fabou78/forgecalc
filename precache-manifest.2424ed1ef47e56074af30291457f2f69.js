aaac873c3ad1d4c007af2d5be4f981aed27492f6
Fabou78 <fpaz@toto.com>
2019-01-31 19:05:52 +0000

    Updates

---
+++ precache-manifest.2424ed1ef47e56074af30291457f2f69.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c0fd2a6a70d681a37ee",
+    "url": "/forgecalc/static/js/runtime~main.5c0fd2a6.js"
+  },
+  {
+    "revision": "9a684092d03b81d56eab",
+    "url": "/forgecalc/static/js/main.9a684092.chunk.js"
+  },
+  {
+    "revision": "c74d7307e620ee670550",
+    "url": "/forgecalc/static/js/1.c74d7307.chunk.js"
+  },
+  {
+    "revision": "a6d219db4abe7ae35d0b202d6735918b",
+    "url": "/forgecalc/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/forgecalc/precache-manifest.2bdb6b74e82057a1a767b87b55480e2d.js"
+  "/forgecalc/precache-manifest.2424ed1ef47e56074af30291457f2f69.js"
 );
 
 workbox.clientsClaim();
