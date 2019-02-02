ea8139a57e3da6c11afc55fb3bc629eb4a505fdd
Fabou78 <fpaz@toto.com>
2019-02-02 17:41:43 +0000

    Updates

--- precache-manifest.58b1918741acf58da7c4b8dca8600e73.js
+++ precache-manifest.58b1918741acf58da7c4b8dca8600e73.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/forgecalc/static/js/runtime~main.5c0fd2a6.js"
   },
   {
-    "revision": "54ec8b6c63ed4998b577",
-    "url": "/forgecalc/static/js/main.54ec8b6c.chunk.js"
+    "revision": "1cae3cc833a1c89d2e29",
+    "url": "/forgecalc/static/js/main.1cae3cc8.chunk.js"
   },
   {
     "revision": "fa021adb6e57eb8c96bf",
     "url": "/forgecalc/static/js/1.fa021adb.chunk.js"
   },
   {
-    "revision": "cfbc78c40dfb862704fc4caee0028ef4",
+    "revision": "eee48e94b38c66d6ddfb2ad1796b4386",
     "url": "/forgecalc/index.html"
   }
 ];
