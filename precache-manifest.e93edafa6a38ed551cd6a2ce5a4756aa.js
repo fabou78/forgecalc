1dda268c62d9729f52e334dd60b13a30ef4fef29
Fabou78 <fpaz@toto.com>
2019-09-30 13:46:43 +0100

    Updates

---
+++ precache-manifest.e93edafa6a38ed551cd6a2ce5a4756aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15bb14316ddf9e357a50c9e33dca67d3",
+    "url": "/forgecalc/index.html"
+  },
+  {
+    "revision": "2b7e25be7109e542714d",
+    "url": "/forgecalc/static/js/2.22dc74a5.chunk.js"
+  },
+  {
+    "revision": "0edfaed5b585afa6bb25",
+    "url": "/forgecalc/static/js/main.8cca1f74.chunk.js"
+  },
+  {
+    "revision": "b3d1e41805acb4e2d5be",
+    "url": "/forgecalc/static/js/runtime-main.8df4e48d.js"
+  }
+]);
