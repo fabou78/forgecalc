7327f196a7f44fa6b12e4f1b1d4047ff78c8bc20
Fabou78 <fpaz@toto.com>
2019-10-14 17:35:55 +0100

    Working piechart without data

--- src/components/Arcstats.jsx
+++ src/components/Arcstats.jsx
@@ -1,13 +1,40 @@
 import React, { Component, } from 'react';
 
+import { PieChart, Pie, Legend, Cell, Tooltip, ResponsiveContainer, Sector,
+  Label, LabelList } from 'recharts';
+import { scaleOrdinal } from 'd3-scale';
+import { schemeCategory10 } from 'd3-scale-chromatic';
+
 import { Paper, Typography, Grid } from '@material-ui/core';
 // import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
-
 import { withStyles } from '@material-ui/core/styles';
-
 import Info from '@material-ui/icons/Info';
-
 import ProgressBar from './Progressbar';
+
+
+// For testing only
+const data02 = [
+  { name: 'No Arc', value: 2400 },
+  { name: 'Lower than 60', value: 4567 },
+  { name: 'Greater than 59', value: 1398 },
+  { name: 'Greater than 79', value: 9800 },
+];
+
+const colors = scaleOrdinal(schemeCategory10).range();
+
+const renderLabelContent = (props) => {
+  const { value, percent, x, y, midAngle } = props;
+  // console.log(props);
+  return (
+    <g transform={`translate(${x}, ${y})`} textAnchor={ (midAngle < -90 || midAngle >= 90) ? 'end' : 'start'}>
+      <text x={0} y={0}>{` Count: ${value}`}</text>
+      <text x={0} y={20}>{` (Percent: ${(percent * 100).toFixed(2)}%)`}</text>
+    </g>
+  );
+};
+
+
+
 
 
 const styles = theme => ({
@@ -50,6 +77,99 @@ const styles = theme => ({
 });
 
 
+
+
+
+
+
+
+export class ArcGraph extends Component {
+
+  static displayName = 'Arc Donut';
+
+  state = {
+    data02: data02,
+    activeIndex: 0,
+    animation: true,
+  };
+
+  render () {
+    const { data02 } = this.state;
+    console.log(this.props);
+    return (
+      <div className="pie-charts">
+        <div className="pie-chart-wrapper">
+          <PieChart width={560} height={400}>
+            <Legend />
+            <Pie
+              data={data02}
+              dataKey="value"
+              cx={250}
+              cy={200}
+              startAngle={180}
+              endAngle={-180}
+              innerRadius={60}
+              outerRadius={90}
+              label={renderLabelContent}
+              paddingAngle={5}
+              isAnimationActive={this.state.animation}
+            >
+              {
+                this.state.data02.map((entry, index) => (
+                  <Cell key={`slice-${index}`} fill={colors[index % 10]}/>
+                ))
+              }
+              <Label width={50} position="center">
+                Arc Status
+              </Label>
+            </Pie>
+          </PieChart>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Arcstasts extends Component {
   state = {
     isLoaded: false,
@@ -58,10 +178,10 @@ class Arcstasts extends Component {
     pcnt_player_tot: '',
     pcnt_player_inac: '',
     nb_player_tot: 0,
-    nb_player_inac: 0    
+    nb_player_inac: 0
   }
 
-  componentDidMount() {    
+  componentDidMount() {
     fetch('https://my-json-server.typicode.com/fabou78/forgecalc/players')
       .then(response => response.json())
       .then(
@@ -70,16 +190,29 @@ class Arcstasts extends Component {
           this.setState({
             isLoaded: true,
             players: result
-          })          
-        });    
+          })
+        });
     fetch('https://my-json-server.typicode.com/fabou78/forgecalc/stats')
       .then(response => response.json())
       .then(
-        (result) => {          
-          var percent1 = (Math.round((result[0].stat / 80) * 100)).toString() + '%';                    
+        (result) => {
+          var percent1 = (Math.round((result[0].stat / 80) * 100)).toString() + '%';
           var nbplay = result[0].stat;
-          var percent2 = (Math.round((result[1].stat / nbplay) * 100)).toString() + '%';               
-          var nbinac = result[1].stat;    
+          var percent2 = (Math.round((result[1].stat / nbplay) * 100)).toString() + '%';
+          var nbinac = result[1].stat;
+          var arc_tot = result[2].stat;
+          var arc_60 = result[3].stat;
+          var arc_80 = result[4].stat;
+          var arc_data =[];
+          var nbactive = nbplay - nbinac
+          var no_arc = nbactive - arc_tot
+          var low_arc = arc_tot - arc_60 - arc_80
+          arc_data.push({ name: 'No Arc', value: no_arc});
+          arc_data.push({ name: 'Lower than 60', value: low_arc });
+          arc_data.push({ name: 'Greater than 59', value: arc_60 });
+          arc_data.push({ name: 'Greater than 79', value: arc_80 });
+          console.log(arc_data);
+
           this.setState({
             isLoaded: true,
             stats: result,
@@ -87,8 +220,8 @@ class Arcstasts extends Component {
             pcnt_player_inac: percent2,
             nb_player_tot: nbplay,
             nb_player_inac: nbinac
-          })          
-        });    
+          })
+        });
   }
 
   render() {
@@ -104,22 +237,26 @@ class Arcstasts extends Component {
               </Typography>
             </Grid>
             <Grid item xs={12}>
-              <Typography variant='subtitle2'>                
+              <Typography variant='subtitle2'>
                 <p fontStyle="italic">A quick look at the Arc situation in the guild... </p>
               </Typography>
             </Grid>
 
-           
+
             <Grid item xs={12}>
-              <p>Total number of players {nb_player_tot}
+              <p>Total number of players : <strong>{nb_player_tot}</strong>
               <ProgressBar percentage={pcnt_player_tot} /></p>
             </Grid>
 
             <Grid item xs={12}>
-              <p>Number of incative players (last 48 hours) {nb_player_inac}
+              <p>Number of incative players (last 48 hours) : <strong>{nb_player_inac}</strong>
               <ProgressBar percentage={pcnt_player_inac} /></p>
             </Grid>
-            
+
+            <Grid item xs={12}>
+              <ArcGraph arc_data={this.arc_data} />
+            </Grid>
+
 
           </Grid> {/* container */}
 
@@ -128,7 +265,7 @@ class Arcstasts extends Component {
               <Typography color='secondary'>
                 <Info className={classes.icon} />
                 <span className={classes.icon}>
-                  &nbsp;Contributions are all based on 1.8 rate (Arc 60+ required to make profit). Numbers will be correct only if no one else outside interfere with the deals.
+                  &nbsp;Inactive players are ignored in Arc statistics.
                 </span>
               </Typography>
             </Grid>
